b33f08e17f1ec0c2f4bbdb241dd7b176242415f2
Julian McNeill <julesdowork@gmail.com>
2018-08-10 13:50:21 -0400

    Persistent SFX

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour {
-    
+
+    [SerializeField] AudioClip coinPickupSfx;
+
     void OnTriggerEnter2D (Collider2D other)
     {
+        AudioSource.PlayClipAtPoint(coinPickupSfx, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
